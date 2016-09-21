ae2883437f4a8fca71d2eabcf60a1b424d7266ff
Daniel <captainmunie@gmail.com>
2016-09-21 11:09:58 -0700

    Character button animations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+
+
+});
+
 // Constants which can be tweaked
 var TO_NEXT_LEVEL = 2; // Determines how soon the next level is reached
 var STARTING_LIVES = 3; // Starting player lives
@@ -1115,6 +1120,7 @@ var changeCharacter = function (characterName) {
     }
 };
 
+// Helper function to adjust collectible counters
 var collectibleRateAdjust = function (type, amount) {
     var flag;
     if (type === "increase") {
@@ -1172,3 +1178,34 @@ var messageUpdater = function (id, newMessage, colorChange) {
     message.innerHTML = newMessage;
     message.style.color = colorChange;
 };
+
+// Initialize with Spot as selected character
+$("#btn-spot").addClass("blue-active");
+
+// Changes button highlighting when user clicks on buttons
+$(".btn").click(function () {
+    var buttonID = $(this).attr("id");
+    $("#btn-spot").removeClass("blue-active");
+    $("#btn-pink").removeClass("red-active");
+    $("#btn-miao").removeClass("green-active");
+    $("#btn-horn-girl").removeClass("purple-active");
+    $("#btn-princess").removeClass("orange-active");
+    switch (buttonID) {
+        case "btn-spot":
+            $(this).addClass("blue-active");
+            break;
+        case "btn-pink":
+            $(this).addClass("red-active");
+            break;
+        case "btn-miao":
+            $(this).addClass("green-active");
+            break;
+        case "btn-horn-girl":
+            $(this).addClass("purple-active");
+            break;
+        case "btn-princess":
+            $(this).addClass("orange-active");
+            break;
+        default:
+    }
+});
