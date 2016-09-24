a2a7e3203c8af6be2f1f2afea48e2b726bbbb6c4
Captain Lin <Captain Lin>
2016-09-23 23:41:03 -0700

    Worked on overlay content

--- js/overlay.js
+++ js/overlay.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	var $overlayContent = $('<div id="overlay-content"></div>');
 	var $overlayTitle = $('<h1>About</h1>');
 	var $exitButton = $('<div><a id="btn-exit" href="#"><i class="fa fa-times-circle-o" aria-hidden="true"></a></i></div>')
+
 	var w = $("html").css("width");
   var h = $("html").css("height");
-
   // Define overlay
 	$("body").append($overlay);
 	$overlay.css({
@@ -23,8 +23,8 @@ $(document).ready(function() {
 	$("body").append($overlayContent);
 	$overlayContent.css({
 		background: "white",
-		width: "700px",
-		height: "900px",
+		width: "600px",
+		height: "800px",
 		display: "none",
 		margin: "auto",
 		"margin-top": "50px",
@@ -35,7 +35,6 @@ $(document).ready(function() {
 	  bottom: "0",
 		"z-index": "100",
 		"border-radius": "10px"
-
 	});
 
 	// Overlay title
@@ -49,28 +48,49 @@ $(document).ready(function() {
 	$overlayContent.append($exitButton);
 	$exitButton.css({
 		"float": "right",
-		"margin-top": "-60px",
-		"margin-right": "10px",
+		"margin-top": "-65px",
+		"margin-right": "12px",
 		"margin-bottom": "0px",
 		"margin-left": "0px",
 		"font-size": "24px"
 	});
 
-	$("#overlayContent").click(function(event) {
-		event.stopPropagation();
+	// Overlay Content
+	$overlayContent.append("<p>This frogger game was made as the final project for Udacity's\
+	<a href='https://www.udacity.com/course/object-oriented-javascript--ud015'>Object-Oriented Javascript</a> course.</p>");
+
+	$overlayContent.append("<p>Use the arrow keys to move your character around. Cross the river, collect items and unlock new characters!</p>");
+
+	$overlayContent.append("<p>Click <a href='#'>here</a> to watch the introduction.</p>");
+
+	$overlayContent.append('<p>*Note* This game works best on a 1920 x 1080 screen. \
+	If you are playing on a smaller screen please adjust your window resolution by holding down the &lt;CTRL&gt; key \
+	and scrolling downwards on the &lt;MOUSEWHEEL&gt;');
+
+	$overlayContent.append('<p>Please help out by sending any errors and bug reports to <a href="mailto:munie.l2p@gmail.com?Subject=Bug%20Report" target="_top">munie.l2p@gmail.com</a>.')
+
+	$overlayContent.append('<p>If you enjoyed the game, please consider donating! Every dollar is very much appreciated.</p>');
+	$overlayContent.append('<a id="donate" class="link" target="_blank" rel="noopener nofollow" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=6KBWFDB4C3EA4"><img src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/gold-pill-paypal-26px.png"></a>')
+
+	// CSS for content in overlay
+	$overlayContent.children().each(function () {
+		$("p").css({
+			width: "75%",
+			margin: "auto",
+			"margin-top": "1.5em",
+			"margin-bottom": "1.5em",
+			"text-align": "left",
+			display: "block",
+			color: "black",
+			"font-size": "1em"
+		});
 	});
 
 	// Shows & hides overlay
 	$("#btn-info, #btn-exit, #overlay").click(function(event) {
 		event.preventDefault();
-		console.log($(this));
-		if ($(this).is("#overlay-content")) {
-			console.log("OVERLAY CONTENT CLICKED");
-		}
 		$overlay.toggle();
 		$overlayContent.toggle();
 	});
-
-
 
 });
